b050b76a181d3b870b65251dcf052bbc0554ccf6
JUN ZHU <junzhu@yahoo.com>
2017-05-31 01:00:32 -0500

    Add skeleton daily processor micro-service

---
+++ daily-processor/src/main/java/com/rainston/dailyprocessor/DailyProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.rainston.dailyprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyProcessorApplication.class, args);
+	}
+}
