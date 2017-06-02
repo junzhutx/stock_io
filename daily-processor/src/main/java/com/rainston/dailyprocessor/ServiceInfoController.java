6b82b2a8562cfcfe08f4ae9ed4650cf9d1f447fc
JUN ZHU <junzhu@yahoo.com>
2017-06-02 00:57:42 -0500

    Update the e2e

---
+++ daily-processor/src/main/java/com/rainston/dailyprocessor/ServiceInfoController.java
@@ -0,0 +1,15 @@
+package com.rainston.dailyprocessor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service-info")
+public class ServiceInfoController {
+
+		@RequestMapping(method=RequestMethod.GET, value="/version")
+		public String getVersion() {
+			return "{ \"version\": \"0.0.1\" }";
+		}
+}
--- ui/angular/src/app/app.component.ts
+++ ui/angular/src/app/app.component.ts
@@ -8,11 +8,21 @@ import { Http } from '@angular/http';
 })
 export class AppComponent {
   spaceScreens: Array<any>;
+  dailyProcessorVersion: string;
+  companyListVersion: string;
 
   constructor(private http: Http) {
     this.http.get('./data.json')
     .map(response => response.json().screenshots)
     .subscribe(res => this.spaceScreens = res);
+
+    this.http.get('stock-io-daily-processor/service-info/version')
+    .map(response => response.json())
+    .subscribe(res => this.dailyProcessorVersion = res);
+
+    this.http.get('stock-io-company-list/service-info/version')
+    .map(response => response.json())
+    .subscribe(res => this.companyListVersion = res);
   }
 
   likeMe(i) {
