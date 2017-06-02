3d3e8f7f68aba60cfd9088dfaf25cd5f8e6a0ca0
JUN ZHU <junzhu@yahoo.com>
2017-06-02 00:14:30 -0500

    Application configuration

--- company-list/src/main/java/com/rainston/companylist/CompanyListApplication.java
+++ company-list/src/main/java/com/rainston/companylist/CompanyListApplication.java
@@ -2,8 +2,10 @@ package com.rainston.companylist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompanyListApplication {
 
 	public static void main(String[] args) {
--- ui/src/main/java/com/rainston/ui/UiApplication.java
+++ ui/src/main/java/com/rainston/ui/UiApplication.java
@@ -2,8 +2,12 @@ package com.rainston.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class UiApplication {
 
 	public static void main(String[] args) {
