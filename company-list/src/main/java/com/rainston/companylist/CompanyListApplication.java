b75a7638a6de87b6c084054d1249295f7bcf1abc
JUN ZHU <junzhu@yahoo.com>
2017-06-01 23:53:07 -0500

    Add skeleton for company-list service

---
+++ company-list/src/main/java/com/rainston/companylist/CompanyListApplication.java
@@ -0,0 +1,12 @@
+package com.rainston.companylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyListApplication.class, args);
+	}
+}
