ce0238257eabb1f26eea62c858c37f816565662e
JUN ZHU <junzhu@yahoo.com>
2017-06-09 18:52:48 -0500

    Fix the spring mongodb repo error

--- company-list/src/main/java/com/rainston/companylist/CompanyListApplication.java
+++ company-list/src/main/java/com/rainston/companylist/CompanyListApplication.java
@@ -3,9 +3,11 @@ package com.rainston.companylist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableMongoRepositories ("com.rainston.common.repository")
 public class CompanyListApplication {
 
 	public static void main(String[] args) {
--- daily-processor/src/main/java/com/rainston/dailyprocessor/DailyProcessorApplication.java
+++ daily-processor/src/main/java/com/rainston/dailyprocessor/DailyProcessorApplication.java
@@ -3,9 +3,11 @@ package com.rainston.dailyprocessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableMongoRepositories ("com.rainston.common.repository")
 public class DailyProcessorApplication {
 
 	public static void main(String[] args) {
