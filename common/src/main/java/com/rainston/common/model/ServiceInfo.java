743fc3a22d3ed949df96c98cacd0a79d9c40605c
JUN ZHU <junzhu@yahoo.com>
2017-06-12 23:00:35 -0500

    Adding classpath info in service info call

--- common/src/main/java/com/rainston/common/model/ServiceInfo.java
+++ common/src/main/java/com/rainston/common/model/ServiceInfo.java
@@ -1,7 +1,10 @@
 package com.rainston.common.model;
 
+import java.util.List;
+
 public class ServiceInfo {
 	String version;
+	List<String> classpath;
 
 	public String getVersion() {
 		return version;
@@ -9,5 +12,13 @@ public class ServiceInfo {
 
 	public void setVersion(String version) {
 		this.version = version;
+	}
+
+	public List<String> getClasspath() {
+		return classpath;
+	}
+
+	public void setClasspath(List<String> classpath) {
+		this.classpath = classpath;
 	}
 }
--- daily-processor/src/main/java/com/rainston/dailyprocessor/controller/ServiceInfoController.java
+++ daily-processor/src/main/java/com/rainston/dailyprocessor/controller/ServiceInfoController.java
@@ -1,5 +1,9 @@
 package com.rainston.dailyprocessor.controller;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +18,15 @@ public class ServiceInfoController {
 		public ServiceInfo getVersion() {
 			ServiceInfo info = new ServiceInfo();
 			info.setVersion("0.0.2");
+			ArrayList<String> cp = new ArrayList<String>();
+			
+			ClassLoader cl = ClassLoader.getSystemClassLoader();
+	        URL[] urls = ((URLClassLoader)cl).getURLs();
+	        for(URL url: urls){
+	        	cp.add(url.getFile());
+	        }
+	        
+	        info.setClasspath(cp);
 			return info;
 		}
 }
