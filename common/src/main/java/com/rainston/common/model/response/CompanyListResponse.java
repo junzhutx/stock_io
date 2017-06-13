ef7b6555257b7b5dbf89435173aa32bf2252f4e1
JUN ZHU <junzhu@yahoo.com>
2017-06-13 00:59:20 -0500

    Add pagination

--- common/src/main/java/com/rainston/common/model/response/CompanyListResponse.java
+++ common/src/main/java/com/rainston/common/model/response/CompanyListResponse.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.rainston.common.model.Company;
 
 public class CompanyListResponse extends BaseResponse {
+	int totalPages;
+	long totalElements;
 	List<Company> companies;
 
 	public List<Company> getCompanies() {
@@ -13,5 +15,21 @@ public class CompanyListResponse extends BaseResponse {
 
 	public void setCompanies(List<Company> companies) {
 		this.companies = companies;
+	}
+
+	public int getTotalPages() {
+		return totalPages;
+	}
+
+	public void setTotalPages(int totalPages) {
+		this.totalPages = totalPages;
+	}
+
+	public long getTotalElements() {
+		return totalElements;
+	}
+
+	public void setTotalElements(long totalElements) {
+		this.totalElements = totalElements;
 	}
 }
--- company-list/src/main/java/com/rainston/companylist/controller/CompanyListController.java
+++ company-list/src/main/java/com/rainston/companylist/controller/CompanyListController.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rainston.common.model.Company;
@@ -25,13 +29,16 @@ public class CompanyListController {
 	CompanyRepository companyRepository;
 	
 	@RequestMapping(value="/lists", method=RequestMethod.POST)
-	public CompanyListResponse getCompanyList() {
+	public CompanyListResponse getCompanyList(@RequestParam("page") int page, @RequestParam("count") int count) {
 		CompanyListResponse response = new CompanyListResponse();
 		response.setErrorCode(ErrorCode.OK);
 		response.setErrorMessage("Success!");
 		
-		List<Company> theList = companyRepository.findAll();
-		response.setCompanies(theList);
+		Pageable p = new PageRequest(page, count);
+		Page<Company> theList = companyRepository.findAll(p);
+		response.setCompanies(theList.getContent());
+		response.setTotalPages(theList.getTotalPages());
+		response.setTotalElements(theList.getTotalElements());
 		return response;
 	}
 
